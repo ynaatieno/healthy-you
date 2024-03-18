53e66dab8e3ced0f11805bcb00f4174a698c306c
yna <ynaooro@gmail.com>
2024-03-18 16:23:04 +0300

    added a paragrah

---
+++ js/healthy.js
@@ -0,0 +1,10 @@
+let button=document.querySelector(".btn")
+let input=document.querySelector(".input")
+// let navs=documents.querySelector(".navigation")
+button.addEventListener("click",function(){
+    console.log(button)
+window.location.href=`interact.html`
+})
+input.addEventListener("click",function(){
+console.log(input)
+})
